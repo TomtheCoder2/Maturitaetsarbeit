86b08ac93974bff48ea04f2590d59fdcc6c9cad1
TomtheCoder2 <jan.wilhelm77@gmail.com>
2024-11-22 22:10:46 +0100

    started on sources

---
+++ documentation/doc.ps1
@@ -0,0 +1,7 @@
+cd out;
+pdflatex ../src/main.tex --interaction=nonstopmode --output-directory=./ --include-directory=../src/;
+bibtex .\main.aux --include-directory=../src/;
+pdflatex ../src/main.tex --interaction=nonstopmode --output-directory=./ --include-directory=../src/;
+pdflatex ../src/main.tex --interaction=nonstopmode --output-directory=./ --include-directory=../src/;
+pdflatex ../src/main.tex --interaction=nonstopmode --output-directory=./ --include-directory=../src/;
+cd ..;
