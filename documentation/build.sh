3a17a37c80565d56ccb03834b3c57d48c4d52518
janwi_mac <jan.wilhelm77@gmail.com>
2025-03-14 23:07:14 +0100

    units smh

--- documentation/build.sh
+++ documentation/build.sh
@@ -1,7 +1,17 @@
+set -e
+
+# File name (without extension)
+filename="main"
+
+# Navigate to the output directory
 cd out;
-pdflatex ../src/diff.tex --interaction=nonstopmode --output-directory=./ --include-directory=../src/;
-bibtex .\diff.aux --include-directory=../src/;
-pdflatex ../src/diff.tex --interaction=nonstopmode --output-directory=./ --include-directory=../src/;
-pdflatex ../src/diff.tex --interaction=nonstopmode --output-directory=./ --include-directory=../src/;
-pdflatex ../src/diff.tex --interaction=nonstopmode --output-directory=./ --include-directory=../src/;
+
+# Compile the LaTeX file
+pdflatex ../src/$filename.tex -interaction=nonstopmode -output-directory=./ -include-directory=../src/ --include-directory=../src/
+bibtex ./$filename.aux --include-directory=../src/
+pdflatex ../src/$filename.tex -interaction=nonstopmode -output-directory=./ -include-directory=../src/ --include-directory=../src/
+pdflatex ../src/$filename.tex -interaction=nonstopmode -output-directory=./ -include-directory=../src/ --include-directory=../src/
+pdflatex ../src/$filename.tex -interaction=nonstopmode -output-directory=./ -include-directory=../src/ --include-directory=../src/
+
+# Return to the original directory
 cd ..;
