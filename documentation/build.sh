cc59b23f9021da6f031e6af1e6b549e430cd01ee
janwi_mac <jan.wilhelm77@gmail.com>
2025-03-15 14:21:21 +0100

    fixing other stuff

--- documentation/build.sh
+++ documentation/build.sh
@@ -1,17 +1,23 @@
-set -e
+#set -e
 
 # File name (without extension)
 filename="main"
 
+rm -r out
+mkdir out
+
 # Navigate to the output directory
-cd out;
+cd src || mkdir src;
 
 # Compile the LaTeX file
-pdflatex ../src/$filename.tex -interaction=nonstopmode -output-directory=./ -include-directory=../src/ --include-directory=../src/
-bibtex ./$filename.aux --include-directory=../src/
-pdflatex ../src/$filename.tex -interaction=nonstopmode -output-directory=./ -include-directory=../src/ --include-directory=../src/
-pdflatex ../src/$filename.tex -interaction=nonstopmode -output-directory=./ -include-directory=../src/ --include-directory=../src/
-pdflatex ../src/$filename.tex -interaction=nonstopmode -output-directory=./ -include-directory=../src/ --include-directory=../src/
+pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=../out/  ./main.tex
+cd ../out || mkdir out;
+cp ../src/sources.bib .;
+bibtex ./$filename.aux
+cd ../src || mkdir src;
+pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=../out/  ./main.tex
+pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=../out/  ./main.tex
+
 
 # Return to the original directory
 cd ..;
